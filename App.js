4f95849f8752765835ea0a6653bc995b8fc1be70
Laras310 <nurullarasati310@gmail.com>
2022-04-20 22:31:29 +0700

    upload app awal

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button, Alert } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as WebBrowser from 'expo-web-browser';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const terscan=({data})=>{
+    console.log(x);
+    setScanned(true);
+    handleBarCodeScanned({data});
+  };
+
+  const handleBarCodeScanned = ({data}) =>
+    Alert.alert(
+      "Scan successful",
+      `Barcode: ${data}. Redirect link?`,  
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log(scanned),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => WebBrowser.openBrowserAsync(data)}
+      ]
+    );
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : terscan}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View>
   );
 }
@@ -13,8 +55,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'column',
     justifyContent: 'center',
   },
 });
